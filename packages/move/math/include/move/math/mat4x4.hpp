37101ca9112cc8d3333224b60e01f39b41909bc0
Ian Diaz <diaz.ian.w@gmail.com>
2024-09-06 02:39:44 -0500

    More fixes

--- packages/move/math/include/move/math/mat4x4.hpp
+++ packages/move/math/include/move/math/mat4x4.hpp
@@ -429,9 +429,9 @@ namespace move::math
             const fast_vec3_t& target,
             const fast_vec3_t& up) noexcept
         {
-            // auto intermediate = mat4
-            return (rtm::ext::look_at_lh<rtm_mat4x4_t, rtm_vec4_t>(
-                eye.to_rtm(), target.to_rtm(), up.normalized().to_rtm()));
+            // mat4x4 intermediate =
+            return rtm::ext::look_at_lh<rtm_mat4x4_t, rtm_vec4_t>(
+                eye.to_rtm(), target.to_rtm(), up.normalized().to_rtm());
 
             // Change unsafe nans to safe nans
             // T data[16];
