a57a0ecea4a6cfad256ea56e8f5ea5e6aaa2081a
Ian Diaz <diaz.ian.w@gmail.com>
2026-03-18 19:47:11 -0700

    Fixed all file omitting several files

--- packages/move/math/include/move/math/all.hpp
+++ packages/move/math/include/move/math/all.hpp
@@ -2,6 +2,10 @@
 
 #include <move/math/common.hpp>
 #include <move/math/macros.hpp>
+#include <move/math/mat3x3.hpp>
+#include <move/math/mat3x4.hpp>
+#include <move/math/mat4x4.hpp>
+#include <move/math/quat.hpp>
 #include <move/math/vec2.hpp>
 #include <move/math/vec3.hpp>
 #include <move/math/vec4.hpp>
